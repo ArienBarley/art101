e80f6da1a1d6bd9eb911996dc36d1ba59c6f3b89
ArienBarley <arienbarley@gmail.com>
2022-10-16 12:31:35 -0700

    Added lab 5 and fixed site-wide background color problems

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/*
+Author: Arien Barley
+Created 16 october
+Licence: Public Domain
+*/
+
+// define variables
+make = "Greedy INC.";
+model = "Four Wheels";
+colour = "Red";
+year = 2012;
+
+//calculate
+age = 2022 - year;
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Colour: " + colour + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
