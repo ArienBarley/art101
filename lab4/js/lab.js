75aa2eff49ea0f5ad6e9af52717f03a64c4adbc0
ArienBarley <arienbarley@gmail.com>
2022-10-17 17:54:29 -0700

    lab 4 and styling improvements

---
+++ lab4/js/lab.js
@@ -0,0 +1,11 @@
+// Loading ascii animation
+// little function to print a spinning line
+// just as an example of psuedocode
+// Author Arien Barley
+// 17/10/2022
+
+// animation = ['|', '/', '—', '\']
+// loop while loading is true:
+//     for charecter in animation:
+//         print charecter
+//         clear output
