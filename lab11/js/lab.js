af1da0d15ad1d72b86fca47cbf3a857441b8b0c1
ArienBarley <arienbarley@gmail.com>
2022-11-03 10:52:05 -0700

    lab 11

---
+++ lab11/js/lab.js
@@ -0,0 +1,73 @@
+/*
+Author: Arien Barley
+Created: 3rd November
+License: Public Domain
+*/
+
+// functions
+//utilities
+function writeToDiv(text, id){
+    //writes the passed text to the specified div
+    document.getElementById(id).innerHTML = text;
+};//end writeToDiv
+
+function arrToString(arr){
+    //returns the array in a better format for
+    //writing in html
+    s = '';
+    for (i=0;i<arr.length;i++){
+        s += arr[i].toString() + "<br>";
+    };//end for
+    return s;
+};//end arrToString
+
+function random_from(lst) {
+  // returns a random item from the passed list
+  return lst[~~(lst.length* Math.random())];
+};//end random_from
+
+function range(end, start = 0, increment = 1){
+    //returns an array incrementing as specified
+    result = [];
+    for (i=start;i<=end; i += increment){
+        result.push(i);
+    }//end for
+    return result
+}//end range
+
+function zip(...args){
+    // takes  arrays as arguments
+    //and returns an array of the form
+    //[ [arr1[0],arr2[0], arr3[0],...], [[arr1[1], arr2[1],arr3[1],...],
+    //[arr1[2], arr2[2], arr3[2],...], ..., [[arr1[n], arr2[n], arr3[n],...] ]
+    var arrays=[];
+    //turn arguments into an array
+    for (var i = 0; i<args.length; i++){
+        arrays.push(args[i]);
+    };
+
+    zipped = arrays[0].map(function(a,i){
+        var result = [a];
+        //step through arrays appending the ith element from
+        //each array to result
+        for (var j = 1; j<arrays.length; j++){
+            result.push(arrays[j][i]);
+        };
+        return result;
+    });
+
+    console.log(zipped)
+    return zipped;
+};//end zip
+
+//end utilities
+
+//functions
+//function create2Buttons(button1){}
+function createButton(){
+        $("#output-1").append("<button class = 'creator-button' > Create More Buttons </button>");
+        $('.creator-button').click(createButton);
+};
+
+//buttons and calls
+$('.creator-button').click(createButton);
